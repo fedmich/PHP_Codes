bd1be7913c63eb6c337d4ed5d09b269c833fcb97
Fedmich <fedmich@gmail.com>
2015-10-30 18:08:10 +0800

    Sample usage and results

--- mask_email.php
+++ mask_email.php
@@ -24,3 +24,33 @@
 			
 		return implode("@", $mail_parts );
 	}
+
+	
+	//Sample usage and results
+	$emails = array(
+		'fed1234@gmail.com'
+		, 'veronica@example.co.uk'
+		
+		, 'fedr@gmail.com'
+		, 'fed@gmail.com'
+		, 'fe@gmail.com'
+		, 'f@gmail.com'
+		, 'f@gmail.com'
+		
+		, 'federicogarcia@gmail.com'
+		, 'federico@testing.ph'
+		
+	
+	);
+	
+	foreach( $emails as $email ){
+		echo " <b>$email</b> ";
+		echo '<br />';
+		
+		$em = mask_email ( $email );
+		echo $em ;
+		echo '<br />';
+
+		echo '<hr />';
+		
+	}
